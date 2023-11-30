fdba13b86c7fa16b188fc0abd3f5162072b1e092
FirstName LastName <mail_adress@example.com>
2023-11-30 23:59:34 +0300

    Home Work 6

--- Home Work.py
+++ Home Work.py
@@ -1,29 +1,100 @@
-#1 Три точки заданы своими координатами X(x1, x2), Y(y1, y2) и Z(z1, z2). Найти и напечатать координаты точки, для которой угол между осью абсцисс и лучом, соединяющим начало координат с точкой, минимальный. Вычисления оформить в виде процедуры.
-import math
-def minimal_angle(x1, x2, y1, y2, z1, z2):
-    angle_x = math.atan2(x2, x1)
-    angle_y = math.atan2(y2, y1)
-    angle_z = math.atan2(z2, z1)
-    min_angle = min(angle_x, angle_y, angle_z)
-    if min_angle == angle_x:
-        return "X", (x1, x2)
-    elif min_angle == angle_y:
-        return "Y", (y1, y2)
-    else:
-        return "Z", (z1, z2)
-x1, x2 = map(float, input("Введите координаты точки X (x1 x2): ").split())
-y1, y2 = map(float, input("Введите координаты точки Y (y1 y2): ").split())
-z1, z2 = map(float, input("Введите координаты точки Z (z1 z2): ").split())
-result = minimal_angle(x1, x2, y1, y2, z1, z2)
-print(f"Точка с минимальным углом: {result[0]} с координатами {result[1]}")
+#1 Дан двумерный массив размером 3x3. Определить максимальное значение среди элементов третьего столбца массива; максимальное значение среди элементов второй строки массива. Вывести полученные значения.
+import random
+rows = 3
+cols = 3
+matrix = [[random.randint(1, 10) for _ in range(cols)] for _ in range(rows)]
+print("Случайная матрица:")
+for row in matrix:
+    print(row)
+max_in_third_column = max(row[2] for row in matrix)
+max_in_second_row = max(matrix[1])
+print("Максимальное значение в третьем столбце:", max_in_third_column)
+print("Максимальное значение во второй строке:", max_in_second_row)
 
 
-#2 Найти все простые натуральные числа, не превосходящие n, двоичная запись которых представляет собой палиндром, т. е. читается одинаково слева направо и справа налево.
-n = int(input('Введите значение:'))
-k = 0
-for i in range(1, n + 1):
-    binary = bin(i)[2:]
-    if binary == binary[::-1]:
-        j = i
-        k += 1
-        print(F'(Ваш по счету полиндром:{k}, Соотвествующее ему знание:{j}')
+#2 Дан двумерный массив размером mxn. Сформировать новый массив заменив положительные элементы единицами, а отрицательные нулями. Вывести оба массива.
+import random
+rows = random.randint(1, 10)
+cols = random.randint(1, 10)
+matrix = [[random.randint(-9, 9) for _ in range(cols)] for _ in range(rows)]
+print("Случайная матрица и изначальная матрица:")
+for row in matrix:
+    print(row)
+print("Новая матрица:")
+new_matrix = [[1 if x > 0 else 0 for x in row] for row in matrix]
+for row in new_matrix:
+    print(row)
+
+
+#3 Дана целая квадратная матрица n-го порядка. Определить, является ли она магическим квадратом, т. е. такой матрицей, в которой суммы элементов во всех строках и столбцах одинаковы.
+import random
+def is_magic_square(matrix):
+    n = len(matrix)
+    expected_sum = sum(matrix[0])
+    # Проверка суммы строк
+    for row in matrix:
+        if sum(row) != expected_sum:
+            return False
+    # Проверка суммы столбцов
+    for j in range(n):
+        if sum(matrix[i][j] for i in range(n)) != expected_sum:
+            return False
+    # Проверка суммы диагоналей
+    if sum(matrix[i][i] for i in range(n)) != expected_sum:
+        return False
+
+    if sum(matrix[i][n - 1 - i] for i in range(n)) != expected_sum:
+        return False
+    return True
+# Размер квадратной матрицы
+n = 3
+# Создание случайной квадратной матрицы
+matrix = [[random.randint(1, 9) for _ in range(n)] for _ in range(n)]
+# Вывод сгенерированной матрицы
+print("Сгенерированная квадратная матрица:")
+for row in matrix:
+    print(row)
+# Проверка, является ли матрица магическим квадратом
+if is_magic_square(matrix):
+    print("Это магический квадрат.")
+else:
+    print("Это не магический квадрат.")
+
+
+#4 Определить, является ли заданная целая квадратная матрица n-го порядка симметричной (относительно главной диагонали).
+def is_symmetric(matrix):
+    n = len(matrix)
+    for i in range(n):
+        for j in range(n):
+            if matrix[i][j] != matrix[j][i]:
+                return False
+    return True
+matrix = [
+    [1, 2, 3],
+    [2, 4, 5],
+    [3, 5, 6]
+]
+if is_symmetric(matrix):
+    print("Матрица симметрична относительно главной диагонали.")
+else:
+    print("Матрица не симметрична относительно главной диагонали.")
+
+
+#5 Дана прямоугольная матрица. Найти строку с наибольшей и строку с наименьшей суммой элементов. Вывести на печать найденные строки и суммы их элементов.
+# Пример прямоугольной матрицы
+import random
+rows = 4
+cols = 4
+matrix = [[random.randint(1, 10) for _ in range(cols)] for _ in range(rows)]
+for row in matrix:
+    print(row)
+max_row = max(matrix, key=sum)
+min_row = min(matrix, key=sum)
+print("Строка с наибольшей суммой элементов:", max_row)
+print("Сумма элементов в этой строке:", sum(max_row))
+print("Строка с наименьшей суммой элементов:", min_row)
+print("Сумма элементов в этой строке:", sum(min_row))
+
+
+#6 Дана действительная матрица размером n х m, все элементы которой различны. В каждой строке выбирается элемент с наименьшим значением. Если число четное, то заменяется нулем, нечетное - единицей. Вывести на экран новую матрицу
+# Простите, не могу приложить ума, как это сделать. К сожалению, я это не осилил... :(
