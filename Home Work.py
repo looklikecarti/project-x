e0794b8902fefa25f6efaeabbc1158a0099f2613
FirstName LastName <mail_adress@example.com>
2023-12-01 00:02:12 +0300

    Home Work 10

--- Home Work.py
+++ Home Work.py
@@ -1,65 +1,65 @@
-#2
+#1 Создать объект pandas Series из листа, объекта NumPy, и словаря
+import pandas as pd
 import numpy as np
+my_list = [1, 2, 3, 4, 5]
+my_array = np.array([10, 20, 30, 40, 50])
+my_dict = {'a': 100, 'b': 200, 'c': 300, 'd': 400, 'e': 500}
+series_from_list = pd.Series(my_list)
+series_from_array = pd.Series(my_array)
+series_from_dict = pd.Series(my_dict)
+print("Series из списка:")
+print(series_from_list)
+print("\nSeries из массива NumPy:")
+print(series_from_array)
+print("\nSeries из словаря:")
+print(series_from_dict)
+
+
+#2 Получить не пересекающиеся элементы в двух объектах Series
+import pandas as pd
+series1 = pd.Series([1, 2, 3, 4, 5])
+series2 = pd.Series([4, 5, 6, 7, 8])
+not_common_in_series1 = series1[~series1.isin(series2)]
+not_common_in_series2 = series2[~series2.isin(series1)]
+print("Элементы, которые есть только в первой Series:")
+print(not_common_in_series1)
+print("\nЭлементы, которые есть только во второй Series:")
+print(not_common_in_series2)
+
+
+#3 Узнать частоту уникальных элементов объекта Series (гистограмма)
+import pandas as pd
 import matplotlib.pyplot as plt
-def f1(x):
-    return x**2
-def f2(x):
-    return np.sqrt(x)
-def f3(x):
-    return -x**2
-x_values = np.linspace(0.01, 10, 400)
-x_values_neg = np.linspace(-10, -0.01, 400)
-plt.figure(figsize=(10, 6))
-plt.plot(x_values, f1(x_values), label='f(x) = f1(x)', color='blue')
-plt.plot(x_values[:50], f1(x_values[:50]), linestyle='dashed', color='blue', label='Small x')
-plt.plot(x_values[-50:], f1(x_values[-50:]), linestyle='dashed', color='blue', label='Large x')
-plt.axhline(y=0, color='black', linestyle='--', label='f(x) = 0')
-plt.xlabel('x')
-plt.ylabel('f(x)')
-plt.title('График для x > 0')
-plt.legend()
-plt.grid(True)
-plt.show()
-plt.figure(figsize=(10, 6))
-plt.plot(x_values_neg, f3(x_values_neg), label='f(x) = f3(x)', color='red')
-plt.plot(x_values_neg, f3(x_values_neg), linestyle='dashed', color='red', label='x -> -∞')
-plt.axhline(y=0, color='black', linestyle='--', label='f(x) = 0')
-plt.xlabel('x')
-plt.ylabel('f(x)')
-plt.title('График для x < 0')
-plt.legend()
-plt.grid(True)
+series = pd.Series([1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5])
+value_counts = series.value_counts()
+value_counts.plot(kind='bar')
+plt.title('Гистограмма уникальных элементов')
+plt.xlabel('Уникальные элементы')
+plt.ylabel('Частота')
 plt.show()
 
 
-#4
-import numpy as np
-import matplotlib.pyplot as plt
-def plot_graph(alpha, beta, color, label):
-    x = np.linspace(-5, 5, 100)
-    y = alpha * x + beta
-    plt.plot(x, y, color, label=label)
-plt.figure(figsize=(15, 5))
-plt.subplot(1, 3, 1)
-plot_graph(1, 0, 'b--', label='α=1, β=0')
-plot_graph(1, -1, 'r--', label='α=1, β=-1')
-plot_graph(1, 0.5, 'g-', label='α=1, β=0.5')
-plot_graph(1, 0.8, 'y-', label='α=1, β=0.8')
-plt.title('График 1')
-plt.legend()
-plt.subplot(1, 3, 2)
-plot_graph(1, 0, 'b--', label='α=1, β=0')
-plot_graph(1, -1, 'r--', label='α=1, β=-1')
-plot_graph(1, -0.5, 'g-', label='α=1, β=-0.5')
-plot_graph(1, -0.8, 'y-', label='α=1, β=-0.8')
-plt.title('График 2')
-plt.legend()
-plt.subplot(1, 3, 3)
-plot_graph(1, 0, 'b--', label='α=1, β=0')
-plot_graph(1, -1, 'r--', label='α=1, β=-1')
-plot_graph(1, -1.5, 'g-', label='α=1, β=-1.5')
-plot_graph(1, -2.5, 'y-', label='α=1, β=-2.5')
-plt.title('График 3')
-plt.legend()
-plt.tight_layout()
-plt.show()
+#4 Объединить два объекта Series вертикально и горизонтально
+import pandas as pd
+series1 = pd.Series([1, 2, 3])
+series2 = pd.Series([4, 5, 6])
+vertical_concatenation = pd.concat([series1, series2])
+print("Вертикальное объединение:")
+print(vertical_concatenation)
+import pandas as pd
+series1 = pd.Series([1, 2, 3])
+series2 = pd.Series([4, 5, 6])
+horizontal_concatenation = pd.concat([series1, series2], axis=1)
+print("Горизонтальное объединение:")
+print(horizontal_concatenation)
+
+
+#5 Найти разность между объектом Series и смещением объекта Series на n
+import pandas as pd
+my_series = pd.Series([1, 3, 5, 7, 9])
+n = 2
+difference = my_series - my_series.shift(n)
+print("Исходная Series:")
+print(my_series)
+print("\nРазность между Series и его смещением на", n, "позиции:")
+print(difference)
