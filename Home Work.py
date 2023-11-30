ea19467a99635b2b1cfbef2128d01cb60967eaf3
FirstName LastName <mail_adress@example.com>
2023-12-01 00:00:47 +0300

    Home work 7

--- Home Work.py
+++ Home Work.py
@@ -1,100 +1,106 @@
-#1 Дан двумерный массив размером 3x3. Определить максимальное значение среди элементов третьего столбца массива; максимальное значение среди элементов второй строки массива. Вывести полученные значения.
-import random
-rows = 3
-cols = 3
-matrix = [[random.randint(1, 10) for _ in range(cols)] for _ in range(rows)]
-print("Случайная матрица:")
-for row in matrix:
-    print(row)
-max_in_third_column = max(row[2] for row in matrix)
-max_in_second_row = max(matrix[1])
-print("Максимальное значение в третьем столбце:", max_in_third_column)
-print("Максимальное значение во второй строке:", max_in_second_row)
+#1 Напишите функцию read_last(lines, file), которая будет открывать определенный файл file и выводить на печать построчно последние строки в количестве lines (на всякий случай проверим, что задано положительное целое число). Протестируем функцию на файле article.txt со следующим содержимым:
+def read_last(lines, file):
+    if not isinstance(lines, int) or lines <= 0:
+        print("Количество строк должно быть положительным целым числом.")
+        return
+    try:
+        with open(file, 'r', encoding='utf-8') as f:
+            lines_list = f.readlines()
+            lines_to_print = lines_list[-lines:]
+            for line in lines_to_print:
+                print(line.strip())
+    except FileNotFoundError:
+        print(f"Файл '{file}' не найден.")
+read_last(6, "article.txt")
 
 
-#2 Дан двумерный массив размером mxn. Сформировать новый массив заменив положительные элементы единицами, а отрицательные нулями. Вывести оба массива.
-import random
-rows = random.randint(1, 10)
-cols = random.randint(1, 10)
-matrix = [[random.randint(-9, 9) for _ in range(cols)] for _ in range(rows)]
-print("Случайная матрица и изначальная матрица:")
-for row in matrix:
-    print(row)
-print("Новая матрица:")
-new_matrix = [[1 if x > 0 else 0 for x in row] for row in matrix]
-for row in new_matrix:
-    print(row)
+#2 Выберите любую папку на своем компьютере, имеющую вложенные директории. Выведите на печать в терминал ее содержимое, как и всех подкаталогов при помощи функции print_docs(directory).
+import os
+def print_docs(directory):
+    for root, dirs, files in os.walk(directory):
+        print(f"Папка: {root}")
+        for file in files:
+            print(f"Файл: {os.path.join(root, file)}")
+print_docs("D:")
+#3 Документ article.txt содержит следующий текст:
+#Требуется реализовать функцию longest_words(file), которая выводит слово, имеющее максимальную длину (или список слов, если таковых несколько).
+def longest_words(file):
+    try:
+        with open(file, 'r', encoding='utf-8') as f:
+            text = f.read()
+            words = text.split()
+            if not words:
+                print("Файл не содержит слов.")
+                return
+            max_length = max(len(word) for word in words)
+            longest_words_list = [word for word in words if len(word) == max_length]
+            if len(longest_words_list) == 1:
+                print(f"Самое длинное слово: {longest_words_list[0]}")
+            else:
+                print("Самые длинные слова:")
+                for word in longest_words_list:
+                    print(word)
+    except FileNotFoundError:
+        print(f"Файл '{file}' не найден.")
+longest_words("article.txt")
+#4.Составьте программу - простейший редактор текстовых файлов. Алгоритм работы программы:
+#Программа предлагает ввести имя будущего файла. Записывает его, присваивая расширение .txt.
+#Программа предлагает записать строку в файл. При каждом нажатии кнопки ENTER происходит запись строки и переход на новую строчку.
+#Если строка пустая или спец. символ - программа сохраняет файл.
+#Требуется создать csv-файл rows_300.csv со следующими столбцами:
+#– № - номер по порядку (от 1 до 300);
+#– Секунда – текущая секунда на вашем ПК;
+#– Микросекунда – текущая миллисекунда на часах.
+#На каждой итерации цикла искусственно приостанавливайте скрипт на 0,01 секунды.
+def text_editor():
+    file_name = input("Введите имя будущего файла (без расширения): ") + ".txt"
+    with open(file_name, 'w', encoding='utf-8') as file:
+        while True:
+            line = input("Введите строку (нажмите Enter для сохранения, либо введите '.' для выхода): ")
+            if line == '.':
+                break
+            file.write(line + '\n')
 
+    print(f"Файл '{file_name}' сохранен.")
+text_editor()
 
-#3 Дана целая квадратная матрица n-го порядка. Определить, является ли она магическим квадратом, т. е. такой матрицей, в которой суммы элементов во всех строках и столбцах одинаковы.
-import random
-def is_magic_square(matrix):
-    n = len(matrix)
-    expected_sum = sum(matrix[0])
-    # Проверка суммы строк
-    for row in matrix:
-        if sum(row) != expected_sum:
-            return False
-    # Проверка суммы столбцов
-    for j in range(n):
-        if sum(matrix[i][j] for i in range(n)) != expected_sum:
-            return False
-    # Проверка суммы диагоналей
-    if sum(matrix[i][i] for i in range(n)) != expected_sum:
-        return False
-
-    if sum(matrix[i][n - 1 - i] for i in range(n)) != expected_sum:
-        return False
-    return True
-# Размер квадратной матрицы
-n = 3
-# Создание случайной квадратной матрицы
-matrix = [[random.randint(1, 9) for _ in range(n)] for _ in range(n)]
-# Вывод сгенерированной матрицы
-print("Сгенерированная квадратная матрица:")
-for row in matrix:
-    print(row)
-# Проверка, является ли матрица магическим квадратом
-if is_magic_square(matrix):
-    print("Это магический квадрат.")
-else:
-    print("Это не магический квадрат.")
-
-
-#4 Определить, является ли заданная целая квадратная матрица n-го порядка симметричной (относительно главной диагонали).
-def is_symmetric(matrix):
-    n = len(matrix)
-    for i in range(n):
-        for j in range(n):
-            if matrix[i][j] != matrix[j][i]:
-                return False
-    return True
-matrix = [
-    [1, 2, 3],
-    [2, 4, 5],
-    [3, 5, 6]
-]
-if is_symmetric(matrix):
-    print("Матрица симметрична относительно главной диагонали.")
-else:
-    print("Матрица не симметрична относительно главной диагонали.")
-
-
-#5 Дана прямоугольная матрица. Найти строку с наибольшей и строку с наименьшей суммой элементов. Вывести на печать найденные строки и суммы их элементов.
-# Пример прямоугольной матрицы
-import random
-rows = 4
-cols = 4
-matrix = [[random.randint(1, 10) for _ in range(cols)] for _ in range(rows)]
-for row in matrix:
-    print(row)
-max_row = max(matrix, key=sum)
-min_row = min(matrix, key=sum)
-print("Строка с наибольшей суммой элементов:", max_row)
-print("Сумма элементов в этой строке:", sum(max_row))
-print("Строка с наименьшей суммой элементов:", min_row)
-print("Сумма элементов в этой строке:", sum(min_row))
-
-
-#6 Дана действительная матрица размером n х m, все элементы которой различны. В каждой строке выбирается элемент с наименьшим значением. Если число четное, то заменяется нулем, нечетное - единицей. Вывести на экран новую матрицу
-# Простите, не могу приложить ума, как это сделать. К сожалению, я это не осилил... :(
+import csv
+import time
+# Создаем CSV-файл
+with open('rows_300.csv', 'w', newline='') as csvfile:
+    fieldnames = ['№', 'Секунда', 'Миллисекунда']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    writer.writeheader()
+    for i in range(1, 301):
+        current_time = time.time()
+        sec = int(current_time)
+        msec = int((current_time - sec) * 1000)
+        writer.writerow({'№': i, 'Секунда': sec, 'Миллисекунда': msec})
+print("CSV-файл 'rows_300.csv' создан.")
+# 5 При помощи библиотеки Pillow в директории circles (создайте ее во время выполнения функции) нарисуйте и сохраните 100 кругов радиусом 300 пикселей случайных цветов в формате jpg на белом фоне (каждый круг - отдельный файл). Для этого напишите функцию circles_generator(num_of_circles=100).
+# from random import randint
+# import os
+# from PIL import Image, ImageDraw
+from random import randint
+import os
+from PIL import Image, ImageDraw
+def circles_generator(num_of_circles=100):
+    # Создайте директорию 'circles', если она не существует
+    if not os.path.exists('circles'):
+        os.makedirs('circles')
+    for i in range(num_of_circles):
+        # Создаем новое изображение с белым фоном
+        image = Image.new('RGB', (600, 600), color='white')
+        # Создаем объект ImageDraw для рисования на изображении
+        draw = ImageDraw.Draw(image)
+        # Генерируем случайный цвет
+        color = (randint(0, 255), randint(0, 255), randint(0, 255))
+        # Генерируем случайные координаты для центра круга
+        x = randint(0, 600)
+        y = randint(0, 600)
+        # Рисуем круг радиусом 300 пикселей с выбранным цветом
+        draw.ellipse((x - 300, y - 300, x + 300, y + 300), fill=color)
+        # Сохраняем изображение как файл с именем, содержащим номер
+        image.save(f'circles/circle_{i+1}.jpg')
+        print(f"Создан файл 'circle_{i+1}.jpg'")
+circles_generator()
